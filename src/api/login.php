2120d5c9d983151a96c2320cfe33209093ba534f
radis toubalidis <rtoumpalidis@gmail.com>
2021-12-06 12:56:01 +0200

    first commit

---
+++ src/api/login.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+    // if already loged in
+    if(isset($_SESSION['loggedIn'])){
+        header('location: ../../Board/index.php');
+        exit();
+    }
+    if(isset($_POST['login'])){
+        include '../config/db.php';
+        // $conn = new mysqli('localhost','adse21','adse21','moutz_tests');
+
+        $username = $conn->real_escape_string($_POST['usernamePHP']);
+        $passwd = strtolower(sha1($conn->real_escape_string($_POST['passwordPHP'])));
+
+        $data = $conn->query("SELECT id from users WHERE username='$username' AND password= '$passwd'");
+        if($data->num_rows > 0){
+            /// everything ok 
+            $_SESSION['loggedIn'] = 1;
+            $_SESSION['username'] = $username;
+            exit('Login successful');
+        }else{
+            exit('username or password were not found!');
+        }
+
+    }
