230d9ab5fe97098d8a9c6cc68b913ef62654b8ec
radis toubalidis <rtoumpalidis@gmail.com>
2021-12-22 18:57:51 +0200

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -7,9 +7,27 @@ CREATE TABLE users(
     losses int(11)  DEFAULT 0
 );
 
-CREATE TABLE deck(
-    title varchar(3)
+CREATE TABLE board(
+  round int PRIMARY KEY,
+  p1_hand varchar(3),
+  p2_hand varchar(3),
+  doubles varchar(1)
 );
 
-INSERT INTO deck(title) VALUES ('AC'), ('AD'), ('AH'), ('AS'), ('2C'), ('2D'), ('2H'), ('2S'), ('3C'), ('3D'), ('3H'), ('3S'), ('4C'), ('4D'), ('4H'), ('4S'), ('5C'), ('5D'), ('5H'), ('5S'), ('6C'), ('6D'), ('6H'), ('6S'), ('7C'), ('7D'), ('7H'), ('7S'), ('8C'), ('8D'), ('8H'), ('8S'), ('9C'), ('9D'), ('9H'), ('9S'), ('10C'), ('10D'), ('10H'), ('10S'), ('KS'); 
+CREATE TABLE game_status(
+    status enum('not active','initialized','started','ended','aborded') NOT NULL DEFAULT 'not active',
+    p_turn enum('1','2') DEFAULT NULL,
+  result enum ('1','2','D') DEFAULT NULL,
+  last_change timestamp NULL DEFAULT NULL
+);
 
+CREATE TABLE deck ( 
+  suit VARCHAR(2) NOT NULL ,
+  value VARCHAR(1) NOT NULL 
+);
+
+INSERT into deck (suit,value) values 
+('♣','A'),('♣','2'),('♣','3'),('♣','4'),('♣','5'),('♣','6'),('♣','7'),('♣','8'),('♣','9'),('♣','10'),
+('♠','A'),('♠','2'),('♠','3'),('♠','4'),('♠','5'),('♠','6'),('♠','7'),('♠','8'),('♠','9'),('♠','10'),('♠','K'),
+('♥','A'),('♥','2'),('♥','3'),('♥','4'),('♥','5'),('♥','6'),('♥','7'),('♥','8'),('♥','9'),('♥','10'),
+('♦','A'),('♦','2'),('♦','3'),('♦','4'),('♦','5'),('♦','6'),('♦','7'),('♦','8'),('♦','9'),('♦','10');
