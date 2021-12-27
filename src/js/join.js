fa82f8204b538b98ac3e37967a0cb0b2b0400c2b
radistoubalidis <rtoumpalidis@gmail.com>
2021-12-27 18:47:45 +0200

    updated sort algorithm works now

--- src/js/join.js
+++ src/js/join.js
@@ -18,10 +18,10 @@ function createListofUsers(data){
         let tr = document.createElement('tr')
         let username = document.createElement('th')
         username.classList.add('col')
-        username.innerHTML = data[i].username
+        username.innerHTML = sortedData[i].username
         losses = document.createElement('th')
         losses.classList.add('col')
-        losses.innerHTML = data[i].losses
+        losses.innerHTML = sortedData[i].losses
         tr.appendChild(username)
         tr.appendChild(losses)
         tbody.appendChild(tr)
@@ -30,12 +30,8 @@ function createListofUsers(data){
 
 
 function sortLosses(data){
-    for(i=0;i<data.length-1;i++){
-        if (data[i].losses > data[i+1].losses){
-            let temp = data[i]
-            data[i] = data[i+1]
-            data[i+1] = temp
-        }
-    }
+    data.sort((a,b) =>{
+        return a.losses - b.losses
+    })
     return data
 }
