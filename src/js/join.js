f6cf348f97cebf78f6d395840c14de4907fa6791
Dimitris Chatzipourganis <dchatzip@yahoo.gr>
2021-12-30 20:58:47 +0200

    css and js changes
    Some changes on board and join game

--- src/js/join.js
+++ src/js/join.js
@@ -1,61 +1,99 @@
-$(document).ready( () => {
-        const url = '../src/api/gofish.php/leaderboard'
-        $.ajax({
-            url : url,
-            type:'GET',
-            dataType:'JSON',
-            success: (data) => {
-                // data is an array of json objects such as
-                /*
-                    [
-                        {
-                            "username":"string",
-                            "wins"    :"int",
-                            "losses"  :"int",
-                        },
-                        {
-                            "username":"string",
-                            "wins"    :"int",
-                            "losses"  :"int",
-                        },
-                        {
-                            "username":"string",
-                            "wins"    :"int",
-                            "losses"  :"int",
-                        }
-                    ]
-                */
-               console.log(data)
-                createListofUsers(data)
-            }
-        })
-        $('#joinGame').on('click',() =>{
-            console.log('join a game')
-        })
-})
+let playersObject;
+let createListofUsersExecutedOnce = false;
 
+$(document).ready(() => {
+  $("#joinGame").click(checkForAvailablePlayers);
+  setInterval(reloadForPlayers, 1000);
+});
 
-function createListofUsers(data){
-    let tbody = document.querySelector('tbody')
-    sortedData = sortLosses(data)
-    for(i=0;i<data.length;i++){
-        let tr = document.createElement('tr')
-        let username = document.createElement('th')
-        username.classList.add('col')
-        username.innerHTML = sortedData[i].username
-        losses = document.createElement('th')
-        losses.classList.add('col')
-        losses.innerHTML = sortedData[i].losses
-        tr.appendChild(username)
-        tr.appendChild(losses)
-        tbody.appendChild(tr)
-    }
+function reloadForPlayers() {
+  const url = "../src/api/gofish.php/leaderboard";
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "JSON",
+    success: (data) => {
+      playersObject = data;
+      createListofUsers(data);
+      refreshLeaderboard();
+    },
+  });
 }
 
+function createListofUsers(data) {
+  //console.log(createListofUsersExecutedOnce);
+  if (createListofUsersExecutedOnce == false) {
+    createListofUsersExecutedOnce = true;
+    $("#joinGame").removeClass("disabled");
+    let tbody = document.querySelector("tbody");
+    if (tbody.children.length > 0) {
+      tbody.remove();
+    }
+    //console.log(tbody);
+    sortedData = sortLosses(data);
+    for (i = 0; i < data.length; i++) {
+      let tr = document.createElement("tr");
+      let username = document.createElement("th");
+      username.classList.add("col");
+      username.innerHTML = sortedData[i].username;
+      losses = document.createElement("th");
+      losses.classList.add("col");
+      losses.innerHTML = sortedData[i].losses;
+      let connected = document.createElement("th");
+      connected.classList.add("col");
+      if (sortedData[i].loggedIn == "1") {
+        connected.innerHTML = "Yes";
+      } else {
+        connected.innerHTML = "No";
+      }
+      tr.appendChild(username);
+      tr.appendChild(losses);
+      tr.appendChild(connected);
+      tbody.appendChild(tr);
+    }
+  }
+}
 
-function sortLosses(data){
-    data.sort((a,b) =>{
-        return a.losses - b.losses
-    })
-    return data
+function sortLosses(data) {
+  data.sort((a, b) => {
+    return a.losses - b.losses;
+  });
+  return data;
+}
+
+function checkForAvailablePlayers(e) {
+  playersAvailable = false;
+  playerName = $("#playerId").text();
+  playerId = $("#playerId").attr("myid");
+  const allPlayers = playersObject.filter(function (player) {
+    return player.username != playerName && player.loggedIn === "1";
+  });
+  //console.log(allPlayers);
+  if (allPlayers.length > 0) {
+    playersAvailable = true;
+  }
+
+  if (!playersAvailable) {
+    e.preventDefault();
+    alert("There are no available players!");
+  }
+}
+
+function refreshLeaderboard() {
+  table = $("#tableBody");
+  tableRows = table[0].children;
+  tableRowsLength = tableRows.length;
+  sortedData = sortLosses(playersObject);
+  //console.log(sortedData);
+  for (i = 0; i < tableRowsLength; i++) {
+    for (j = 0; j < tableRows[i].cells.length; j++) {
+      tableRows[i].cells[1].innerHTML = sortedData[i].losses;
+      tableRows[i].cells[0].innerHTML = sortedData[i].username;
+      if (sortedData[i].loggedIn == "1") {
+        tableRows[i].cells[2].innerHTML = "Yes";
+      } else {
+        tableRows[i].cells[2].innerHTML = "No";
+      }
+    }
+  }
 }
