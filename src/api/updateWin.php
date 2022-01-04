d01f81631e0293dcd35bba3061206e99fb83792a
Lian48 <51327572+Lian48@users.noreply.github.com>
2022-01-04 14:28:23 +0200

    newUpdateQuery

--- src/api/updateWin.php
+++ src/api/updateWin.php
@@ -5,13 +5,49 @@
         session_start();
     }
 
-    //Pulling the active(logged-in) player by his username
-    $user=$_SESSION["username"];
+    function UpdateWin(){
 
-    //Capturing Wins-Losses to get edited. 
-    $wins=$_POST["wins"];
-    $losses=$_POST["losses"];
-    
+        //Pulling the active(logged-in) player by his username
+        $user=$_SESSION['uid'];
+
+        /*Capturing Wins-Losses to get edited. 
+        $wins=$_POST["wins"];
+        $losses=$_POST["losses"];
+        */
+
+        include '../config/db.php';
+
+        $query_WIN = "UPDATE users
+                      SET wins = wins+1
+                      WHERE id = ?";
+        $stmt=$conn->prepare($query_WIN);
+        $stmt->bind_param("i", $user);
+        $stmt->execute();
+    }
+
+    function UpdateLoss(){
+
+        //Pulling the active(logged-in) player by his username
+        $user=$_SESSION['uid'];
+
+        /*Capturing Wins-Losses to get edited. 
+        $wins=$_POST["wins"];
+        $losses=$_POST["losses"];
+        */
+
+        include '../config/db.php';
+
+        $query_LOSS = "UPDATE users
+                       SET losses = losses+1
+                       WHERE id = ?";
+        $stmt=$conn->prepare($query_LOSS);
+        $stmt->bind_param("i", $user);
+        $stmt->execute();
+    }
+
+    UpdateWin();
+
+    /*
     function UpdateWinLosses(){
         include '../config/db.php';
         
@@ -27,4 +63,4 @@
         //Running Query
         mysqli_query($conn, $query_WIN);
         mysqli_query($conn, $query_LOSS);
-    }
+    }*/
